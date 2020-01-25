90c0e6c87cabc8dd2463bd1c7ba295f92420a271
Chris Linstid <clinstid@gmail.com>
2020-01-25 17:16:16 -0500

    Move swapfiles, backup, Notes creation before we need them

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ln -svf $(pwd)/.vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/swapfiles ~/.vim/backup
+mkdir -p ~/Notes
 ln -svf $(pwd)/ftplugin ~/.vim/ftplugin
 vim +PlugInstall +qall
-mkdir -p ~/.vim/swapfiles ~/.vim/backup
